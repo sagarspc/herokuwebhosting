d8d739bd18ce62c42f4e0a8f754f4bc38ceea4c6
sagarspc <dattaram.120@gmail.com>
2021-12-25 10:47:05 +0530

    website hosting

---
+++ app/middleware/authJwt.js
@@ -0,0 +1,109 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+//console.log(token)
+  if (token) {
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) {
+        res.json({
+          success: false,
+          message: 'Failed to authenticate token'
+        });
+      } else {
+        //console.log(decoded)
+       req.decoded = decoded;
+       //req.decoded = decoded.userId;
+      // req.user = 
+        next();
+
+      }
+    });
+
+  } else {
+    res.status(403).json({
+      success: false,
+      message: 'No token provided'
+    });
+
+  }
+};
+
+isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.userId);
+    const roles = await user.getRoles();
+
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "admin") {
+        return next();
+      }
+    }
+
+    return res.status(403).send({
+      message: "Require Admin Role!",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Unable to validate User role!",
+    });
+  }
+};
+
+isModerator = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.userId);
+    const roles = await user.getRoles();
+
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "moderator") {
+        return next();
+      }
+    }
+
+    return res.status(403).send({
+      message: "Require Moderator Role!",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Unable to validate Moderator role!",
+    });
+  }
+};
+
+isModeratorOrAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.userId);
+    const roles = await user.getRoles();
+
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "moderator") {
+        return next();
+      }
+
+      if (roles[i].name === "admin") {
+        return next();
+      }
+    }
+
+    return res.status(403).send({
+      message: "Require Moderator or Admin Role!",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: "Unable to validate Moderator or Admin role!",
+    });
+  }
+};
+
+const authJwt = {
+  verifyToken:verifyToken,
+  isAdmin,
+  isModerator,
+  isModeratorOrAdmin,
+};
+module.exports = authJwt;
