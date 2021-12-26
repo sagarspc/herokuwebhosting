63765948ad6b328bcbec4d098b435733c352fe69
sagarspc <dattaram.120@gmail.com>
2021-12-26 14:58:34 +0530

    adding some changes in config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  HOST: "184.168.114.28",
+  HOST: "localhost",
   USER: "surajX9",
   PASSWORD: "u].vwyt^Z$f5",
   DB: "testdb_suraj",
   dialect: "mysql",
-  port: 3306,
   pool: {
     max: 5,
     min: 0,
